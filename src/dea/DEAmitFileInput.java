9094057cb94c7d2659d784f7b38a1a3b88c1ae89
Tubinator <walle36@live.de>
2018-10-02 22:40:13 +0200

    -DeltaDefenieren mit geringen Problemen implementiert -Versuche an Menschen sind genehmigt.

--- src/dea/DEAmitFileInput.java
+++ src/dea/DEAmitFileInput.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Hashtable;
 
 /**
  *
@@ -72,7 +73,7 @@ public class DEAmitFileInput {
      */
     public ArrayList BananaSplit(String Zeile) {
         String[] gesplitted = Zeile.split("\\[");
-        
+
         String[] finalSplit = gesplitted[1].split("\\]");
         String[] superfinalSplit = finalSplit[0].split("\\,");
         return new ArrayList<>(Arrays.asList(superfinalSplit));
@@ -210,7 +211,7 @@ public class DEAmitFileInput {
     }
 
     public void ObjektToString() {
-        
+
         System.out.println("Zustaende:");
         ArrayList<String> List = this.getZustaende();
         for (int i = 0; i < List.size(); i++) {
@@ -224,19 +225,75 @@ public class DEAmitFileInput {
         }
 
         //Delta fehlt(Datentyp unklar).
-        
         System.out.println("Q0:" + this.getQ0());
-        
-        
-        
+
         System.out.println("Endzustände:");
         List = this.getEndzustaende();
         for (int i = 0; i < List.size(); i++) {
             System.out.println(List.get(i));
         }
-        
-        
 
+    }
+    /**
+     * Diese Methode...
+     * Sie nimmt die Delta-Zeile entgegen und gibt eine ArrayList mit Hashtables
+     * zurück. In der ArrayList symbolisiert jeder index einen Zustand(i:0 -> q1]
+     * .die Hashtable gibt nun an was passieren soll wenn bestimmte Keys
+     * (Transitionen) auf Grundlage der erlaubten Buchstaben des Aplhabets) 
+     * eingegeben werden.
+     * -ACHTUNG-ATTENTION-ATTENTION-ATTENZIONE
+     * DIESE METHODE DARF ERST AUSGEFÜHRT WERDEN WENN DIE ZUSTEANDE BEREITS
+     * DEFENIERT SIND!
+     * 
+     * 
+     * 
+     * 
+     * 
+     * @param zeile
+     * @return 
+     */
+
+    public ArrayList<Hashtable> DeltaDefenieren(String zeile) {
+        try{
+        ArrayList<String> zustaende = this.getZustaende();
+        ArrayList<Hashtable> allehashtable = new ArrayList<Hashtable>();
+        //Für jeden Zustand wird eine neue Hashtable angelegt.
+        for (String zustand : zustaende) {
+            allehashtable.add(new Hashtable<String, Integer>());
+        }
+
+        String[] gesplitted = zeile.split("\\[");
+        gesplitted = gesplitted[1].split("\\]");
+        gesplitted = gesplitted[0].split("\\;");
+        for (String transition : gesplitted) {
+            String[] stringsplit = transition.split("->");
+            String[] klammersplit = stringsplit[0].split("\\(");
+            klammersplit = klammersplit[1].split("\\)");
+            klammersplit = klammersplit[0].split("\\,");
+
+            for (int i = 0; i < zustaende.size(); i++) {
+                if (zustaende.get(i).equalsIgnoreCase(klammersplit[0])) {
+
+                    System.out.println("stringsplit" + Arrays.toString(stringsplit));//Just Debugging
+                    System.out.println("klammersplit:" + Arrays.toString(klammersplit));//Just Debugging
+                    allehashtable.get(i).put(klammersplit[1], stringsplit[1]);
+
+                }
+
+            }
+
+        }
+
+        return allehashtable;
+        }catch(Exception e){
+            System.out.println("\u001B[31mDie Methode DeltaDefenieren benötigt die "+ 
+                  "Zustände des DEA.Bitte erst BananaSplit auf die Zustände "
+                    + "anwenden");
+            
+            
+          return null;  
+        }
+   
     }
 
     public static void main(String args[]) {
@@ -251,12 +308,8 @@ public class DEAmitFileInput {
         }*/
         DEAmitFileInput d = new DEAmitFileInput();
         ArrayList<String> DateiInArray = d.readFile("TestDEA.txt");
-        d.MethodenWählen(DateiInArray);
-        d.ObjektToString();
         
-        
-        
-        
+        System.out.println(d.DeltaDefenieren(DateiInArray.get(2)));
 
     }
 
